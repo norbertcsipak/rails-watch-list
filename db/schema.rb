18b793302118bab9ba20a5e6d3037bf7bec0a76d
Norbert Csipak <norbertcipak@me.com>
2022-07-28 11:47:18 +0100

    changed db to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,13 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_07_26_192950) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "bookmarks", force: :cascade do |t|
     t.text "comment"
-    t.integer "movie_id", null: false
-    t.integer "list_id", null: false
+    t.bigint "movie_id", null: false
+    t.bigint "list_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["list_id"], name: "index_bookmarks_on_list_id"
@@ -39,7 +42,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_26_192950) do
   create_table "reviews", force: :cascade do |t|
     t.text "comment"
     t.integer "rating"
-    t.integer "list_id", null: false
+    t.bigint "list_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["list_id"], name: "index_reviews_on_list_id"
